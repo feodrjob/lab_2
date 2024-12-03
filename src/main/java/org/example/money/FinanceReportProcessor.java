0afb8b9f976119e319ed5f392a9d4a312c146ec3
Fedorjob <77617920+feodrjob@users.noreply.github.com>
2024-12-03 22:34:41 +0600

    доделан FinanceReportProcessor

--- src/main/java/org/example/money/FinanceReportProcessor.java
+++ src/main/java/org/example/money/FinanceReportProcessor.java
@@ -8,9 +8,23 @@ public class FinanceReportProcessor {
          for (int i =0; i < report.getQuantityPayments();i++){
              if (report.getPayment(i).getFio().charAt(0) == someChars){
                  payments[j] = report.getPayment(i);
+                 j++;
              }
          }
          return new FinanceReport(payments,"Балтийский Дмитрий Главдиректорович","26.11.24");
+    }
+
+    public static FinanceReport paymentsLessThanASpecifiedNumber(FinanceReport report, int SpecifiedNumber){
+        Payment[] payments = new Payment[report.getQuantityPayments()];
+        int j = 0;
+
+        for(int i = 0; i<report.getQuantityPayments();i++){
+            if(report.getPayment(i).getSumPay()<SpecifiedNumber){
+                payments[j]=report.getPayment(i);
+                j++;
+            }
+        }
+        return new FinanceReport(payments,"Балтийский Дмитрий Главдиректорович","26.11.24");
     }
 
 }
