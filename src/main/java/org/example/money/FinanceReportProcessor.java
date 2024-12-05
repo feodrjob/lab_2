5ea313e6a43b78e30893b0ef5dcac400b3a263d9
Fedorjob <77617920+feodrjob@users.noreply.github.com>
2024-12-05 15:44:15 +0600

    доделан FinanceReportProcessor

--- src/main/java/org/example/money/FinanceReportProcessor.java
+++ src/main/java/org/example/money/FinanceReportProcessor.java
@@ -1,30 +1,44 @@
 package org.example.money;
 
 public class FinanceReportProcessor {
-    public static FinanceReport getSceondnamesForFirstChar(FinanceReport report, char someChars){
-        Payment[] payments = new Payment[report.getQuantityPayments()];
-         int j = 0;
-
-         for (int i =0; i < report.getQuantityPayments();i++){
-             if (report.getPayment(i).getFio().charAt(0) == someChars){
-                 payments[j] = report.getPayment(i);
-                 j++;
-             }
-         }
-         return new FinanceReport(payments,"Балтийский Дмитрий Главдиректорович","26.11.24");
-    }
-
-    public static FinanceReport paymentsLessThanASpecifiedNumber(FinanceReport report, int SpecifiedNumber){
-        Payment[] payments = new Payment[report.getQuantityPayments()];
+    public static FinanceReport getSecondNamesForFirstChar(FinanceReport report, char someChars) {
         int j = 0;
-
-        for(int i = 0; i<report.getQuantityPayments();i++){
-            if(report.getPayment(i).getSumPay()<SpecifiedNumber){
-                payments[j]=report.getPayment(i);
+        for (int i = 0; i < report.getQuantityPayments(); i++) {
+            if (report.getPayment(i).getFio().charAt(0) == someChars) {
                 j++;
             }
         }
-        return new FinanceReport(payments,"Балтийский Дмитрий Главдиректорович","26.11.24");
+        Payment[] payments = new Payment[j];
+        int index = 0;
+        for (int i = 0; i < report.getQuantityPayments(); i++) {
+            if (report.getPayment(i).getFio().charAt(0) == someChars) {
+                payments[index] = report.getPayment(i);
+                index++;
+            }
+        }
+        return new FinanceReport(payments, report.getFio(), report.getDate());
+    }
+
+    // ArrayList
+    // два раза пройтись циклом подсчитать количество и создать массив фиксированного размера
+
+
+    public static FinanceReport paymentsLessThanASpecifiedNumber(FinanceReport report, int specifiedNumber) {
+        int j = 0;
+        for (int i = 0; i < report.getQuantityPayments(); i++) {
+            if (report.getPayment(i).getSumPay() < specifiedNumber) {
+                j++;
+            }
+        }
+        int index = 0;
+        Payment[] payments = new Payment[j];
+        for (int i= 0;i<report.getQuantityPayments();i++){
+            if(report.getPayment(i).getSumPay()<specifiedNumber){
+                payments[index] = report.getPayment(i);
+                index++;
+            }
+        }
+        return new FinanceReport(payments,report.getFio(),report.getDate());
     }
 
 }
